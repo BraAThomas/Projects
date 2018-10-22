5b75613c3ddaca507da7691b1150f9ae30fed910
thomasbr <brandonthomas3333@yahoo.com>
2018-10-22 12:11:50 -0500

    Renaming of files

---
+++ GitTester/src/gitTester/Test.java
@@ -0,0 +1,12 @@
+package gitTester;
+
+public class Test {
+
+	//This is a program to test the set up I have created from
+	//eclipse to a git repository.
+	//Brandon Thomas 22 October 2018
+	
+	public static void main(String[] args) {
+		System.out.println("I can print to console.");
+	}
+}
